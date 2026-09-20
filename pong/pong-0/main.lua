89bfdd64e95be07ed3dec8920bf4106c08373df0
Sanjana <rsanjana3028@gmail.com>
2026-09-20 22:40:12 +0530

    Add files via upload

---
+++ pong/pong-0/main.lua
@@ -0,0 +1,22 @@
+WINDOW_WIDTH = 720
+WINDOW_HEIGHT = 360
+
+
+function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+
+function love.draw()
+    love.graphics.printf(
+        'Hello Pong!',
+        0,
+        WINDOW_HEIGHT / 2 - 6,
+        WINDOW_WIDTH,
+        'center'
+    )
+end
